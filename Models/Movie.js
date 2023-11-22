96f5a99bf26670dc4f96534a6591c3c5c9111a47
Pablo Vacas <pablovacasgarcia@gmail.com>
2023-11-22 12:20:30 +0100

    Creada la estructura MVC

---
+++ Models/Movie.js
@@ -0,0 +1,8 @@
+class Movie {
+    constructor(title, year, imdbId, poster) {
+      this.title = title;
+      this.year = year;
+      this.imdbId = imdbId;
+      this.poster = poster;
+    }
+}
---
+++ Models/MovieList.js
@@ -0,0 +1,5 @@
+class MovieList {
+    constructor(){
+        this.movies=[];
+    }
+}
