8bb7b24c77b6851af61c945854c1a82405e2a1b6
Pablo Vacas <pablovacasgarcia@gmail.com>
2023-11-23 22:33:24 +0100

    Update MovieList.js

--- Models/MovieList.js
+++ Models/MovieList.js
@@ -2,4 +2,12 @@ class MovieList {
     constructor(){
         this.movies=[];
     }
+
+    addMovie(movie){
+        this.movies.push(movie);
+    }
+
+    searchMovies(title, page){
+        
+    }
 }
